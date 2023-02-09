39b1ec9dbecb958ca2ff7cbcefa16a434ccfbf9b
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-02-09 20:34:13 +0000

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -10,124 +10,124 @@
 #include <thread>
 
 void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
-                      GLsizei length, GLchar const *message,
-                      void const *user_param){
-  if (severity == GL_DEBUG_SEVERITY_NOTIFICATION){
+                        GLsizei length, GLchar const *message,
+                        void const *user_param){
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION){
     return;
-  }
+    }
 
-  auto const src_str = [source](){
+    auto const src_str = [source](){
     switch(source){
-    case GL_DEBUG_SOURCE_API:
-      return "API";
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-      return "WINDOW SYSTEM";
-    case GL_DEBUG_SOURCE_SHADER_COMPILER:
-      return "SHADER COMPILER";
-    case GL_DEBUG_SOURCE_THIRD_PARTY:
-      return "THIRD PARTY";
-    case GL_DEBUG_SOURCE_APPLICATION:
-      return "APPLICATION";
-    case GL_DEBUG_SOURCE_OTHER:
-      return "OTHER";
-    default:
-      return "UNKNOWN SOURCE";
+        case GL_DEBUG_SOURCE_API:
+            return "API";
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            return "WINDOW SYSTEM";
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            return "SHADER COMPILER";
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            return "THIRD PARTY";
+        case GL_DEBUG_SOURCE_APPLICATION:
+            return "APPLICATION";
+        case GL_DEBUG_SOURCE_OTHER:
+            return "OTHER";
+        default:
+            return "UNKNOWN SOURCE";
     }
-  }();
+    }();
 
-  auto const type_str = [type](){
+    auto const type_str = [type](){
     switch(type){
-    case GL_DEBUG_TYPE_ERROR:
-      return "ERROR";
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-      return "DEPRECATED_BEHAVIOR";
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-      return "UNDEFINED_BEHAVIOR";
-    case GL_DEBUG_TYPE_PORTABILITY:
-      return "PORTABILITY";
-    case GL_DEBUG_TYPE_PERFORMANCE:
-      return "PERFORMANCE";
-    case GL_DEBUG_TYPE_MARKER:
-      return "MARKER";
-    case GL_DEBUG_TYPE_OTHER:
-      return "OTHER";
-    default:
-      return "UNKNOWN TYPE";
+        case GL_DEBUG_TYPE_ERROR:
+            return "ERROR";
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            return "DEPRECATED_BEHAVIOR";
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            return "UNDEFINED_BEHAVIOR";
+        case GL_DEBUG_TYPE_PORTABILITY:
+            return "PORTABILITY";
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            return "PERFORMANCE";
+        case GL_DEBUG_TYPE_MARKER:
+            return "MARKER";
+        case GL_DEBUG_TYPE_OTHER:
+            return "OTHER";
+        default:
+            return "UNKNOWN TYPE";
     }
-  }();
+    }();
 
-  auto const severity_str = [severity](){
+    auto const severity_str = [severity](){
     switch(severity){
-    case GL_DEBUG_SEVERITY_NOTIFICATION:
-      return "NOTIFICATION";
-    case GL_DEBUG_SEVERITY_LOW:
-      return "LOW";
-    case GL_DEBUG_SEVERITY_MEDIUM:
-      return "MEDIUM";
-    case GL_DEBUG_SEVERITY_HIGH:
-      return "HIGH";
-    default:
-      return "UNKNOWN SEVERITY";
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            return "NOTIFICATION";
+        case GL_DEBUG_SEVERITY_LOW:
+            return "LOW";
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            return "MEDIUM";
+        case GL_DEBUG_SEVERITY_HIGH:
+            return "HIGH";
+        default:
+            return "UNKNOWN SEVERITY";
     }
-  }();
-  std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
+    }();
+    std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
 }
 
 int main(int argc, char *argv[]){
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  auto window = glfwCreateWindow(800, 600, "Example", nullptr, nullptr);
-  if(!window){
-    throw std::runtime_error("Error creating glfw window");
-  }
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
-
-  if(!gladLoaderLoadGL()){
-    throw std::runtime_error("Error initializing glad");
-  }
-
-  glDebugMessageCallback(message_callback, nullptr);
-  
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init("#version 450 core");
-  ImGui::StyleColorsClassic();
-
-  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
-  while(!glfwWindowShouldClose(window)){
-    glfwPollEvents();
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    static bool showDemo = false;
-    ImGui::Begin("Example");
-    if(ImGui::Button("Show/Hide ImGui demo")){
-      showDemo = !showDemo;
+    auto window = glfwCreateWindow(800, 600, "Example", nullptr, nullptr);
+    if(!window){
+        throw std::runtime_error("Error creating glfw window");
     }
-    ImGui::End();
-    if(showDemo){
-      ImGui::ShowDemoWindow(&showDemo);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if(!gladLoaderLoadGL()){
+        throw std::runtime_error("Error initializing glad");
     }
 
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    glfwSwapBuffers(window);
-    glClear(GL_COLOR_BUFFER_BIT);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Not sure why this is here but someone clearly added it for a reason so ill keep it lol
-  }
+    glDebugMessageCallback(message_callback, nullptr);
 
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  ImGui::DestroyContext();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 450 core");
+    ImGui::StyleColorsClassic();
 
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        static bool showDemo = false;
+        ImGui::Begin("Example");
+        if(ImGui::Button("Show/Hide ImGui demo")){
+            showDemo = !showDemo;
+        }
+        ImGui::End();
+        if(showDemo){
+            ImGui::ShowDemoWindow(&showDemo);
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glClear(GL_COLOR_BUFFER_BIT);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Not sure why this is here but someone clearly added it for a reason so ill keep it lol
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
